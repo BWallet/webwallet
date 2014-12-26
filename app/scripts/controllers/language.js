2387d3ce57f3adeeaf06ef815d5cc43d1fea91ad
Dean Liu <dean123363@qq.com>
2014-12-25 19:29:50 -0800

    Fixed the bug of logo in prodoction mode

--- app/scripts/controllers/language.js
+++ app/scripts/controllers/language.js
@@ -27,9 +27,18 @@ angular.module('webwalletApp').controller('LanguageCtrl', function (
         console.log('[LanguageCtrl] current language: ', args.language);
         $rootScope.language = localeToLanguage(args.language);
         
+        /*
         $translate('logo-img').then(function (translation) {
             angular.element("#logo-img").attr('src', translation);
         });
+        */
+        if ($rootScope.language == CHINESE) {
+        	angular.element("#logo-img-english").hide();
+        	angular.element("#logo-img-chinese").show();
+        } else {
+        	angular.element("#logo-img-chinese").hide();
+        	angular.element("#logo-img-english").show();
+        }
         
         $translate(['nav-wallet', 'nav-what', 'nav-news', 'nav-help', 'nav-support', 'nav-language']).then(function (translations) {
             for ( var t in translations ){
