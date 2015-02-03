8060dbeb205b879a489a5580dad67a7aa066569e
Dean Liu <dean123363@qq.com>
2015-02-03 01:01:35 -0800

    Added plugin install guide in English

--- app/scripts/controllers/error.js
+++ app/scripts/controllers/error.js
@@ -16,6 +16,7 @@ angular.module('webwalletApp').controller('ErrorCtrl', function (
     trezorApi,
     trezorError,
     $scope,
+    $rootScope,
     $translate) {
 
     'use strict';
@@ -48,6 +49,7 @@ angular.module('webwalletApp').controller('ErrorCtrl', function (
         if (trezorError.installed == false) {
             $scope.javaLink = trezorError.javaLink;
         }
+        
         var installMessageKey = 'error.install.message';
         var pluginMessageKey = 'error.plugin-message';
         var env = trezorError.env;
@@ -67,7 +69,27 @@ angular.module('webwalletApp').controller('ErrorCtrl', function (
         $scope.installMessageKey = installMessageKey;
         $scope.pluginMessageKey = pluginMessageKey;
         
-        var guideUrl = 'http://mybwallet.com/docs/java-guide/zh/';
+        if ($rootScope.language) 
+    	    setupGuideURLs($rootScope.language, env);
+        else
+            setupGuideURLs('english', env);
+        $rootScope.$watch('language',function(){
+    	    setupGuideURLs($rootScope.language, env);
+        });
+    }
+    
+    function setupGuideURLs(language, env) {
+        var guideUrl = 'http://mybwallet.com/docs/plugin-guide/';
+        var anchorEnableJava, anchorEnableApplet; 
+        if (language == 'chinese') {
+        	guideUrl += 'zh/';
+        	anchorEnableJava = 'id3';
+        	anchorEnableApplet = 'bwallet';
+        } else {
+        	guideUrl += 'en/';
+        	anchorEnableJava = 'enable-java-in-browser';
+        	anchorEnableApplet = 'enable-bwallet-plugin';
+        }
         var guidePlatformUrl = guideUrl;
         if (env.os.indexOf('Win') >= 0) {
             guidePlatformUrl += 'win';
@@ -86,9 +108,8 @@ angular.module('webwalletApp').controller('ErrorCtrl', function (
             else 
             	guidePlatformUrl += '-chrome.html';
         }
-        
-        var guideEnableUrl = guidePlatformUrl + "#id3";
-        var guideEnableAppletUrl = guidePlatformUrl + "#bwallet";
+        var guideEnableUrl = guidePlatformUrl + "#" + anchorEnableJava;
+        var guideEnableAppletUrl = guidePlatformUrl + "#" + anchorEnableApplet;
         var guideSuitableUrl = guideUrl + "suitable-java.html";
         $scope.guideURLs = {};
         $scope.guideURLs.platform = guidePlatformUrl;
